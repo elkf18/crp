2bfe56992a04e06da379202bdbfe3375c63ecfdc
Elya Kumala <83533622+elkf18@users.noreply.github.com>
2024-06-07 09:22:40 +0700

    Delete coba/coba-elya directory

--- coba/coba-elya/App/screens/image.js
+++
@@ -1,8 +0,0 @@
-const animImages={
-    pertama:require('../assets/vec5.png'),
-    kedua:require('../assets/vec6.png'),
-    ketiga:require('../assets/vec7.png'),
-    logo:require('../assets/logo.png'),
-    wive:require('../assets/G.png'),
-}
-export default animImages;
